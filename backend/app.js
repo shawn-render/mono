fd042a4501039bfbe8e5d8a27ff4d7fee0df22e8
John Beynon <john@render.com>
2022-10-14 10:56:00 +0000

    Fix paths

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/api', (req, res) => {
+app.get('/hello', (req, res) => {
 	res.json('Hello World, from express');
 });
 
