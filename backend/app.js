73718584e7b47839b4a16ba29ddd28d7e0379e31
matt maddex <mmaddex@render.com>
2022-12-23 13:02:26 -0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get('/hello', (req, res) => {
-	res.json('hey hey, from express');
+	res.json('hey hey hey, from express');
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
