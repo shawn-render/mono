6d272c74bce678afbc612441a42ce697518bc4eb
mmaddex <mmaddex@render.com>
2022-12-23 11:16:37 -0800

    a proper change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>        
+        <p>good bye from 2022</p>
       </header>
     </div>
   );
